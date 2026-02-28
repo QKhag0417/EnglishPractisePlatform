0b782049323ec97197b130fb5942edbf48d87948
Duy <duyhuanhuy@gmail.com>
2026-02-28 21:27:23 +0700

    feat: add UserPracticeSubmissionAnswerListCreateRequestDto for handling bulk answer submissions

---
+++ ielts-mastermind-backend/src/main/java/com/ieltsmastermind/practice/attempt/management/domain/dto/UserPracticeSubmissionAnswerBulkCreateRequestDto.java
@@ -0,0 +1,15 @@
+package com.ieltsmastermind.practice.attempt.management.domain.dto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class UserPracticeSubmissionAnswerListCreateRequestDto {
+
+    private List<UserPracticeSubmissionAnswerCreateRequestDto> answers = new ArrayList<>();
+}
